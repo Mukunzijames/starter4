225af965f891e1083cd648d8e976a76debf9ccda
unknown <mukunzindahiro@gmail.com>
2023-05-14 07:59:36 -0700

    commit ghee

---
+++ hello.js
@@ -0,0 +1,12 @@
+var helloSpeaker = {};
+
+  
+  var speakWord = "Hello";
+
+  
+  helloSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.helloSpeaker = SpeakHello;
+(window);
